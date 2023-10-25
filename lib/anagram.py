39f042d0b72ae000341ef9db6b6926803f6458a5
Tiana Lopez <tianadavin@gmail.com>
2023-10-24 21:55:43 -0700

    finishes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+
+class Anagram:
+
+  def __init__(self, word):
+    self.word = word
+
+  def match(self, word_list):
+    anagram_list = []
+    match_list = list(self.word)
+    for el in word_list:
+      potential_match = list(el)
+      match_list.sort()
+      potential_match.sort()
+      if match_list == potential_match:
+        anagram_list.append(el)
+      else:
+        return anagram_list
